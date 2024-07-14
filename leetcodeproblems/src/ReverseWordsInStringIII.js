de6c126f34cb3e86c871d927460186b40e692cf7
Satish Chakkirala <cvsatish.net@example.com>
2024-07-13 23:14:26 -0700

    reversewordsinstring

--- leetcodeproblems/src/ReverseWordsInStringIII.js
+++ leetcodeproblems/src/ReverseWordsInStringIII.js
@@ -1,28 +1,5 @@
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
-function reverseWordsInAStringSimplify(str) {
-  return str
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .join(" ");
-}
-
-// O(N) time complexity
-function reverseWordsInAString(s) {
-  let result = "";
-  let word = "";
-  for (let char of s) {
-    if (char === " ") {
-      result += word + char;
-      word = "";
-    } else {
-      // this is prepending char to word, so that it prints in reverse.
-      word = char + word;
-    }
-  }
-  result += word; // Add the last word
-  return result;
-}
-
+// https://lucid.app/lucidchart/75d44fef-c1e3-4af6-8cc3-4b8fc147dee3/edit?beaconFlowId=5BB6B70C0261A3D0&invitationId=inv_b97ecb4f-b5b4-48e3-aaa2-236da01f677e&page=0_0#
 //   To reverse the order of characters in each word within a sentence while preserving whitespace and initial word order, we can follow these steps:
 // 1. Split the sentence into words based on spaces.
 // 2. For each word, reverse the characters using the two-pointer approach as described previously. This ensures that each word is reversed in \(O(N/2)\) time complexity, where \(N\) is the length of the word.
@@ -36,32 +13,74 @@ function reverseWordsInAString(s) {
 // where \(N\) is the length of the word, and since we process each word 
 // separately, the overall complexity remains efficient for the task.
 
-// O(N/2) time complexity.
-const reverseWordsInAStringBest = (s) => {
-  // Split the sentence into words
-  let words = s.split(" ");
+// O(N) time complexity.
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    let words = [];
+    words = splitStringToWords(s);
 
-  // Reverse each word
-  for (let i = 0; i < words.length; i++) {
-    words[i] = reverseString(words[i]);
-  }
+    for (let i = 0; i < words.length; i++) {
+        words[i] = reverseWord(words[i])
+    }
+    return words.join(" ");
+};
 
-  // Join the reversed words back into a sentence
-  return words.join(" ");
+const splitStringToWords = (s) => {
+    let word = '';
+    let words = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== ' ') {
+            word += s[i];
+        }
+        else {
+            words.push(word);
+            word = ''
+        }
+    }
+    // add last word
+    if (word.length > 0) {
+        words.push(word);
+    }
+    return words;
 }
 
-function reverseString(word) {
-  let chars = word.split("");
-  let start = 0;
-  let end = word.length - 1;
+const reverseWord = (word) => {
+    let chars = word.split("");
+    let startIndex = 0;
+    let endIndex = word.length - 1;
 
-  while (start < end) {
-    // Swap characters
-    [chars[start], chars[end]] = [chars[end], chars[start]];
-
-    start++;
-    end--;
-  }
-
-  return chars.join("");
+    while (startIndex < endIndex) {
+        [chars[startIndex], chars[endIndex]] = [chars[endIndex], chars[startIndex]];
+        startIndex++;
+        endIndex--;
+    }
+    return chars.join("");
 }
+
+
+function reverseWordsInAStringUsingJavascriptInBuiltFuncitons(str) {
+    return str
+      .split(" ")
+      .map((word) => word.split("").reverse().join(""))
+      .join(" ");
+  }
+  
+  // O(N) time complexity
+  function reverseWordsInAString(s) {
+    let result = "";
+    let word = "";
+    for (let char of s) {
+      if (char === " ") {
+        result += word + char;
+        word = "";
+      } else {
+        // this is prepending char to word, so that it prints in reverse.
+        word = char + word;
+      }
+    }
+    result += word; // Add the last word
+    return result;
+  }
