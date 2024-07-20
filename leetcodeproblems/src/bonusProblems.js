a18f6ec16841c01831e59fbbcba99edd9e841271
Satish Chakkirala <cvsatish.net@example.com>
2024-07-19 23:47:12 -0700

    leet code

--- leetcodeproblems/src/bonusProblems.js
+++ leetcodeproblems/src/bonusProblems.js
@@ -124,7 +124,8 @@ var getCommonValue = function(nums1, nums2) {
 var moveZeroes = function (nums) {
     let write = 0;
     let n = nums.length;
-
+    // [0,1,0,3,2]
+    // output [1,3,2,3,2].
     for (let read = 0; read < n; read++) {
         if (nums[read] !== 0) {
             if (read !== write) { // Check if read and write are different
@@ -134,7 +135,70 @@ var moveZeroes = function (nums) {
         }
     }
 
+    // after the loop, write is at 3
+    // Fill the rest of the array with 0
+    // from write 3 to 5 fill with 0x
+    // output [1,3,2,0,0]
     while (write < n) {
         nums[write++] = 0;
     }
 };
+
+// Question: Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+// For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+// Return the resulting string.
+// Example 1:
+// Input: word = "abcdefd", ch = "d"
+// Output: "dcbaefd"
+// Explanation: The first occurrence of "d" is at index 3.
+// Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+// Example 2:
+// Input: word = "xyxzxe", ch = "z"
+// Output: "zxyxxe"
+// Explanation: The first and only occurrence of "z" is at index 3.
+// Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+// Example 3:
+// Input: word = "abcd", ch = "z"
+// Output: "abcd"
+// Explanation: "z" does not exist in word.
+// You should not do any reverse operation, the resulting string is "abcd".
+// Constraints:
+// 1 <= word.length <= 250
+// word consists of lowercase English letters.
+// ch is a lowercase English letter.
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function (word, ch) {
+    let chFound = false;
+    let chIndex = -1;
+    let result = "";
+  
+    // Step 2: Find `ch` position
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === ch && !chFound) {
+        chIndex = i;
+        chFound = true;
+        break; // Exit loop after finding the first occurrence of `ch`
+      }
+    }
+  
+    // If `ch` is not found, return the original word
+    if (!chFound) return word;
+  
+    // Step 3: Reverse Prefix
+    for (let i = chIndex; i >= 0; i--) {
+      result += word[i];
+    }
+  
+    // Step 4: Append Remaining
+    for (let i = chIndex + 1; i < word.length; i++) {
+      result += word[i];
+    }
+  
+    // Step 5: Return Result
+    return result;
+  };
+  
