72e1da29c554899e6743a5d912b999a2f8e72751
Satish Chakkirala <satish_chakkirala@intuit.com>
2024-10-11 22:43:53 -0700

    leet code

--- leetcodeproblems/src/Queues/Queues.js
+++ leetcodeproblems/src/Queues/Queues.js
@@ -92,3 +92,91 @@ RecentCounter.prototype.ping = function(t) {
  * var obj = new RecentCounter()
  * var param_1 = obj.ping(t)
  */
+
+
+[1, 100, 3001, 4000]
+const RecentCounter = (t) =>{
+  let queue = [];
+  let result = [];
+
+  // queue = [3001,4]
+  const ping = () =>{
+   
+    while( queue.length >0 && t - queue[0] > 3000){
+      queue.shift();
+    }
+    queue.push(t);
+
+    return queue.length;
+  }
+
+  return ping;
+}
+
+// Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+// Implement the MovingAverage class:
+
+// MovingAverage(int size) Initializes the object with the size of the window size.
+// double next(int val) Returns the moving average of the last size values of the stream.
+
+// Example 1:
+
+// Input
+// ["MovingAverage", "next", "next", "next", "next"]
+// [[3], [1], [10], [3], [5]]
+// Output
+// [null, 1.0, 5.5, 4.66667, 6.0]
+
+// Explanation
+// MovingAverage movingAverage = new MovingAverage(3);
+// movingAverage.next(1); // return 1.0 = 1 / 1
+// movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+// movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+// movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+
+// Constraints:
+
+// 1 <= size <= 1000
+// -105 <= val <= 105
+// At most 104 calls will be made to next.
+class MovingAverage {
+  constructor(size) {
+    this.size = size;
+    this.queue = [];
+    this.queueSum = 0;
+  }
+
+  next(val) {
+    // Add new value
+    this.queue.push(val);
+    this.queueSum += val;
+
+    // Remove oldest value if queue exceeds size
+    if (this.queue.length > this.size) {
+      this.queueSum -= this.queue.shift();
+    }
+
+    // Calculate average based on the current queue size
+    return this.queueSum / this.queue.length;
+  }
+}
+
+// alternative
+// Usage
+// const movingAverage = MovingAverage(3);
+function MovingAverageFunction(size) {
+  let queue = [];
+  let queueSum = 0;
+
+  return {
+    next(val) {
+      queue.push(val);
+      queueSum += val;
+      if (queue.length > size) {
+        queueSum -= queue.shift();
+      }
+      return queueSum / queue.length;
+    }
+  };
+}
